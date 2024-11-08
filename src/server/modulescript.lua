deeb9846bc51f52d4631542724bbef5deebf8640
coardz <jericbanalz@gmail.com>
2024-11-08 23:28:59 +0800

    backup for computer reformat

---
+++ src/server/modulescript.lua
@@ -0,0 +1,17 @@
+-- src/ServerScriptService/MyModule.lua
+
+-- local module = {}
+
+-- function module.initialize()
+--     print("Module initialized!")
+-- end
+
+-- return module
+
+local module = {}
+    module.Figter =  {
+        MaxHealth = 100;
+        Power = 30;
+        Speed = 25;
+    }
+    return module
